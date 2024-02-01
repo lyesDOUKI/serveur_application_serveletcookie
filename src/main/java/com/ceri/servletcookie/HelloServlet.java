6b9a55f9996cd9b0935d3780392fdc47b342ab9f
lyesdki <lyes.douki@alumni.univ-avignon.fr>
2024-02-01 10:40:01 +0100

    ajout cookies

--- src/main/java/com/ceri/servletcookie/HelloServlet.java
+++ src/main/java/com/ceri/servletcookie/HelloServlet.java
@@ -6,15 +6,29 @@ import javax.servlet.annotation.*;
 
 
 public class HelloServlet extends HttpServlet {
-    private String message;
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        //recuperation du cookie
+        Cookie[] cookies = request.getCookies();
+        String nom = null;
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("nom")){
+                    nom = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if(nom != null && !nom.isEmpty()){
+            out.println("<h1>Bonjour via le cookies nom : " + nom + "</h1>");
+        }
         //creation formulaire
         out.println("<html><body>");
-        out.println("<form method='post' action=''>");
+        out.println("<form method='get' action='second-servlet'>");
         out.println("<label for='nom'>Nom : </label>");
         out.println("<input type='text' name='nom' id='nom' />");
         out.println("<input type='submit' value='Envoyer' />");
--- src/main/java/com/ceri/servletcookie/SecondServlet.java
+++ src/main/java/com/ceri/servletcookie/SecondServlet.java
@@ -1,4 +1,42 @@
 package com.ceri.servletcookie;
 
-public class SecondServlet {
+
+import java.io.*;
+import javax.servlet.http.*;
+public class SecondServlet extends HttpServlet{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter out = response.getWriter();
+        //recuperation du nom
+        String nom = request.getParameter("nom");
+        if(nom == null || nom.isEmpty()){
+            nom = "inconnu";
+            //suppression cookie
+            //recuperation des cookies et mettre à 0 la durée de vie
+            Cookie[] cookies = request.getCookies();
+            if(cookies != null){
+                for(Cookie cookie : cookies){
+                    if(cookie.getName().equals("nom")){
+                        cookie.setMaxAge(0); // mettre à 0 pour
+                        // le supprimer du navigateur
+                        response.addCookie(cookie);
+                        break;
+                    }
+                }
+            }
+        }else {
+            Cookie cookie = new Cookie("nom", nom);
+            response.addCookie(cookie);
+        }
+        //ajout cookie
+
+        response.setContentType("text/html");
+        out.println("<html><body>");
+        out.println("<h1>Bonjour " + nom + "</h1>");
+        out.println("<a href='hello-servlet'>Retour à helloServlet</a>");
+        out.println("<br><a href='second-servlet?nom="
+                + nom + "'>Réafficher la page</a>");
+        out.println("</body></html>");
+    }
 }
